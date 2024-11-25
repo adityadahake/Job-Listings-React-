eb106a45532ceb5c013ca40113f931744c717c48
Aditya Vinod Dahake <aditya_vd@pursuitsoftware.com>
2024-11-25 12:22:11 +0000

    setup font style and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "desaturated-dark-cyan-primary": "#5BA4A4",
+      "light-grayish-cyan-bg": "#F2F8F8",
+      "light-grayish-cyan-badge": "#EEF6F6",
+      "dark-grayish-cyan": "#7B8E8E",
+      "very-dark-grayish-cyan": "#2C3A3A",
+    },
     extend: {},
   },
   plugins: [],
