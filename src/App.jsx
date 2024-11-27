604cf5486db77c40213d291870bfaad79669c14c
Aditya Vinod Dahake <aditya_vd@pursuitsoftware.com>
2024-11-27 17:45:52 +0530

    add filteration logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import JobListing from "./components/JobListing";
 import TagFilterBar from "./components/TagFilterBar";
 
-import { listingsArray } from "./assets/data/data";
+import JobListingsData from "./assets/data/data.json";
 
 function App() {
   const [activeTags, setActiveTags] = useState([]);
+  const [jobListings, setJobListings] = useState([]);
+  const [originalList, setOriginalList] = useState([]);
 
-  const listings = listingsArray;
+  // Get job listings - import from json or API call
+  useEffect(() => {
+    setOriginalList(JobListingsData);
+    setJobListings(JobListingsData);
+  }, []);
+
+  const filterListings = () => {
+    let filteredList = originalList;
+    activeTags.forEach((tag) => {
+      filteredList = filteredList.filter(
+        (job) =>
+          tag === job.role ||
+          tag === job.level ||
+          job.languages.includes(tag) ||
+          job.tools.includes(tag)
+      );
+    });
+    setJobListings(filteredList);
+  };
 
   return (
     <div className="min-h-screen bg-desaturated-dark-cyan-primary bg-opacity-15 relative">
@@ -29,10 +49,16 @@ function App() {
       ) : (
         <div className="h-12"></div>
       )}
+      <div
+        onClick={filterListings}
+        className="m-3 w-24 text-center rounded cursor-pointer bg-very-dark-grayish-cyan hover:bg-desaturated-dark-cyan-primary text-very-light-cyan"
+      >
+        Filter
+      </div>
       {/* Listings */}
       <div className="container lg:max-w-[1200px] mx-auto">
         <div className="flex flex-col gap-12 lg:gap-10 p-6">
-          {listings.map((listing) => (
+          {jobListings.map((listing) => (
             <JobListing
               key={listing.id}
               listing={listing}
